447b4502d3a6c62e347e51dfc36d973346871cc9
PedroZ <sdustpedro@gmail.com>
2025-09-16 19:43:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,6 +79,8 @@ if [[ -d $BASE_PATH/action_build ]]; then
     BUILD_DIR="action_build"
 fi
 
+chmod +x $BASE_PATH/update.sh
+
 $BASE_PATH/update.sh "$REPO_URL" "$REPO_BRANCH" "$BASE_PATH/$BUILD_DIR" "$COMMIT_HASH"
 
 apply_config
